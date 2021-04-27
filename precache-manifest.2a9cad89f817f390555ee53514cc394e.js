60db05e8f7d4abadca9f9dea27f42466b977c128
Francisco <franciscoparisalbero@gmail.com>
2021-04-27 13:55:11 +0200

    Updates

---
+++ precache-manifest.2a9cad89f817f390555ee53514cc394e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/header-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b41b7a466c746d3ffde6",
+    "url": "/header-react/static/js/runtime~main.b41b7a46.js"
+  },
+  {
+    "revision": "7d34246bdf44ff8534f8",
+    "url": "/header-react/static/js/main.7d34246b.chunk.js"
+  },
+  {
+    "revision": "9559dd4ef738e0fa4c87",
+    "url": "/header-react/static/js/1.9559dd4e.chunk.js"
+  },
+  {
+    "revision": "7d34246bdf44ff8534f8",
+    "url": "/header-react/static/css/main.b93947be.chunk.css"
+  },
+  {
+    "revision": "d68f0e716417087bb84b8ef5af7c08a5",
+    "url": "/header-react/index.html"
+  }
+];
